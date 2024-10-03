c2c931bcf3ef17b75ed50e8d9f87cbb4df40c24d
kevokagwima <kevokagwima@gmail.com>
2024-10-03 01:38:19 -0700

    Updated Files

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
-class Square():
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
     def __init__(self, size):
-        self._size = size
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
+
+"""Define a class Square."""
+
+
 class Square:
-    def __init__(self, size=0):
-        self.__size = size
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
 
     @property
     def size(self):
+        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
@@ -16,9 +28,11 @@ class Square:
         self.__size = value
 
     def area(self):
-        return (self._size * self._size)
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
 
-    def print(self):
+    def my_print(self):
+        """Print the square with the # character."""
         for i in range(0, self.__size):
             [print("#", end="") for j in range(self.__size)]
             print("")
