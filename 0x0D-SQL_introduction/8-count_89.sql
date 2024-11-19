5056abfcdf8aca240033bbcb2ae4326489baea1b
kevokagwima <kevokagwima@gmail.com>
2024-11-19 09:05:38 -0800

    updated files

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
--- script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server
+-- script that displays the number of records
 select count(*) from first_name where id=1;
