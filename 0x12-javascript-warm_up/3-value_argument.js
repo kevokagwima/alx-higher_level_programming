848b2c2312bc0c01eebbefcedd8a5e8b58510c6b
kevokagwima <kevokagwima@gmail.com>
2024-12-17 23:02:36 -0800

    updated Files

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-if (process.argv) {
-	console.log(process.argv);
+if (process.argv[2] === undefined) {
+  console.log('No argument');
 } else {
-	console.log('No argument');
+  console.log(process.argv[2]);
+}
