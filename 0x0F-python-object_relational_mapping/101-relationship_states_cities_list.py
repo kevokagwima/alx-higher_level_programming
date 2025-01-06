f8b29694b48ef629aaef6b03bc0a11b1a94ff5a1
kevokagwima <kevokagwima@gmail.com>
2025-01-06 04:09:27 -0800

    Updated files

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """ prints the State object with the name passed as argument from the database
 """
@@ -21,4 +20,3 @@ if __name__ == "__main__":
         for city_ins in instance.cities:
             print("    ", end="")
             print(city_ins.id, city_ins.name, sep=": ")
-
